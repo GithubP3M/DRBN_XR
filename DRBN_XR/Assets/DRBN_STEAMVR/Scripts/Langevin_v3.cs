556992456e6cdf76b7264f2ef263d6e08c12649c
P3M <11472926+GithubP3M@users.noreply.github.com>
2026-03-16 17:40:20 +0100

    Merging OG Durabin and JS Durabin done (#7)
    * Checkpoint 29/10/2025
    
    * Checkpoint 3/11/25
    
    * Checkpoint 3/11/25
    
    * Im tired
    
    * Checkpoint 3/11/25
    
    * Backup 12/03/26
    
    * .gitignore is now working
    
    * Trying to make it work
    
    * Trying to make it work
    
    * Remove big files
    
    * Remove big files
    
    * Please work
    
    * Please work
    
    ---------
    
    Co-authored-by: Joel Sanchez <joel.sanchez.moran@gmail.com>
    Co-authored-by: Joel Sanchez <joel-isai.sanchez-moran@univ-reims.fr>

--- DRBN_XR/Assets/DRBN_STEAMVR/Scripts/Langevin_v3.cs
+++ DRBN_XR/Assets/DRBN_STEAMVR/Scripts/Langevin_v3.cs
@@ -1,11 +1,3 @@
-﻿using UnityEngine;
-using System.Collections.Generic;
-using System;
-using System.Linq;
-/*using IronPython.Hosting;
-using Microsoft.Scripting.Hosting;*/
-
-public class Langevin_v3 : MonoBehaviour
-{
-
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:69399efdb460c6740c79f5225e7df681f5122ae11557b69ca49d4446c0ed404f
+size 2630
